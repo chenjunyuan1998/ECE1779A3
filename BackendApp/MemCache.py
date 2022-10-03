9a2c266758e9ac22d11e13025f2cc9607c43b223
chenjunyuan1998 <chenjunyuan1998@hotmail.com>
2022-10-02 21:17:59 -0400

    add commented and slight modify to function

--- BackendApp/MemCache.py
+++ BackendApp/MemCache.py
@@ -12,8 +12,14 @@ class Node:
 
 # (O (1) access time)
 class MemCache:
-    # Policy is restricted to LRU and RANDOM
-    # Cap is capacity in MB
+    """
+    Policy is restricted to LRU and RANDOM
+    Cap is capacity in bytes
+    Cache : a dictionary which value is a Node
+    Size : the number of items in the memCache
+    Space : memory usage
+    """
+
     def __init__(self, cap, policy):
         self.left = Node(0, 0)
         self.right = None(0, 0)
@@ -29,7 +35,11 @@ class MemCache:
         self.hit = 0
         self.missed = 0
         self.total = 0
-
+    """
+    Helper function:
+    Always inserted to the right most position in Double LinkedList
+    To make it most recent
+    """
     def insert(self, node):
         node.prev = self.right.prev
         node.next = self.right
@@ -37,10 +47,23 @@ class MemCache:
         self.right.prev.next = node
         self.right.prev = node
 
+    """
+    Helper function:
+    Delete the appearance of the node in the Double LinkedList
+    """
     def remove(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
 
+    """
+    If key not in memcache
+    return false
+    missed request += 1
+    
+    Else for LRU, re-insert the node to last position in Double Linked
+    For recent use
+    Return the value
+    """
     def get(self, key):
         if key not in self.cache:
             self.missed += 1
@@ -53,6 +76,17 @@ class MemCache:
         self.hit += 1
         return self.cache[key].value
 
+    """
+    If the size of the input val is larger than the cap itself
+    return false which means no matter what we cannot put the val into the memcache
+    
+    Else if the key is already exist in memcache
+    First delete its appearance in the ListList and reduce the size 
+    Create a new Node of key, val and insert it into Linkedlist and Cache
+    
+    If the space is larger than capacity
+    Depends on the policy, we randomly delete/delete LRU in the cache until the space is enough
+    """
     def put(self, key, val):
         if sys.getsizeof(val) > self.cap:
             self.missed += 1
@@ -79,17 +113,24 @@ class MemCache:
                 self.invalidateKey(random_key)
 
         return True
-
+    """
+    Clear Everything in the Cache
+    """
     def clear(self):
         self.space = 0
         self.cache.clear()
         self.right.prev = self.left
         self.left.next = self.right
-        self.hit += 1
-        self.total += 1
 
+    """
+    Remove value from Cache
+    Decrease size, space of Cache
+    Remove it from the dictionary
+    """
     def invalidateKey(self, key):
+        self.total += 1
         if key not in self.cache:
+            self.missed += 1
             return False
 
         removed = self.cache[key]
@@ -97,6 +138,7 @@ class MemCache:
         self.remove(removed)
         del self.cache[removed.key]
         self.size -= 1
+        self.hit += 1
         return True
 
     def switchToLRU(self):
@@ -107,17 +149,17 @@ class MemCache:
         if self.policy == 'LRU':
             self.policy = 'RANDOM'
 
-    def missRate(self):
+    def getMissRate(self):
         return self.missed / self.total if self.total != 0 else 0
 
-    def hitRate(self):
+    def getHitRate(self):
         return self.hit / self.total if self.total != 0 else 0
 
     def getItemInCache(self):
         return self.size
 
-    def getSpaceLeft(self):
-        return self.cap - self.space
+    def getCapacityLeft(self):
+        return (self.cap - self.space) // (1024 ** 2)
 
     def getSpace(self):
         return self.space
