145867d5acdc201f32e25a43779b72de0023108c
Yijing622 <chenyijing0622@163.com>
2022-10-06 21:00:43 -0400

    Added database configuration.

---
+++ local/config.py
@@ -0,0 +1,5 @@
+db_config = {'user': 'admin',
+             'password': 'password',
+             'host': 'database-ece1779.c53pbcwxyccy.us-east-2.rds.amazonaws.com',
+             'port': '3306',
+             'database': 'database-ece1779'}
