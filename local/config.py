3bb0a1eb4a888fcb1e109ecedba8d7222a82aa97
Yijing622 <chenyijing0622@163.com>
2022-10-10 18:14:06 -0400

    Completed local testing version.

--- local/config.py
+++ local/config.py
@@ -1,5 +1,12 @@
-db_config = {'user': 'admin',
-             'password': 'password',
-             'host': 'database-ece1779.c53pbcwxyccy.us-east-2.rds.amazonaws.com',
-             'port': '3306',
-             'database': 'database-ece1779'}
+# RDS Config
+# db_config = {'user': 'admin',
+#              'password': 'password',
+#              'host': 'database-ece1779.c53pbcwxyccy.us-east-2.rds.amazonaws.com',
+#              'port': '3306',
+#              'database': 'database-ece1779'}
+
+# Local Config
+db_config = {'user': 'root',
+             'password': 'YOUR_MYSQL_SERVER_PASSWORD',
+             'host': '127.0.0.1',
+             'database': 'ImageDB'}
