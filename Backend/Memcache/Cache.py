d9dbbc31f7650ed65c407b01366da9dc29f3749e
chenjunyuan1998 <chenjunyuan1998@hotmail.com>
2022-12-12 14:56:01 -0500

    create cache functionality

--- Backend/Memcache/Cache.py
+++ Backend/Memcache/Cache.py
@@ -2,54 +2,90 @@ import sys
 from collections import defaultdict
 from collections import OrderedDict
 
-GET_COUNT_THRESHOLD = 10
-
 # key: username, value: set of persistent keys
-persistent_dict = defaultdict(set)
-lru_dict = defaultdict(OrderedDict)
-capacity_dict = dict()
-space_dict = dict()
+class Cache:
+    def __init__(self):
+        self.persistent_key = defaultdict(set)
+        self.presistent_store = defaultdict(dict)
+        self.lru_dict = defaultdict(OrderedDict)
+        self.capacity_dict = defaultdict(int)
+        self.space_dict = defaultdict(int)
+        self.count_dict = defaultdict(dict)
+
+    def set_cap(self, username, capacity):
+        self.capacity_dict[username] = capacity * 1024 * 1024
+
+    def findFirst(self,ordDict):
+        return next(iter(ordDict))
+
+    def put_key(self,username, key, value):
+        if username not in self.capacity_dict:
+            return -1
+
+        self.count_dict[username][key] += 1
+
+        if key not in self.persistent_key[username]:
+            if key in self.lru_dict[username]:
+                self.lru_dict[username].move_to_end(key)
+                self.space_dict[username] -= sys.getsizeof(self.lru_dict[username][key])
+
+            self.lru_dict[username][key] = value
+            self.space_dict[username] += sys.getsizeof(value)
+
+            while self.space_dict[username] > self.capacity_dict[username]:
+                if not self.lru_dict[username]:
+                    return 0
+                    # all space is allocated for presistent data, ask user to delete mannually
+                popped = self.lru_dict[username].popitem(last=False)
+                self.space_dict[username] -= sys.getsizeof(popped[1])
+
+        else:
+            self.space_dict[username] -= sys.getsizeof(self.presistent_store[username][key])
+            self.presistent_store[username][key] = value
+            self.space_dict[username] += sys.getsizeof(value)
+            if self.space_dict[username] > self.capacity_dict[username]:
+                self.space_dict[username] -= sys.getsizeof(self.presistent_store[username][key])
+                del self.presistent_store[username][key]
+                return 0
+
+        self.addToPersistent(self, username, key)
+
+        return 1
+
+    def collectCount(self, username, key):
+        return self.count_dict[username][key]
+
+    def addToPersistent(self, username, key):
+        if self.collectCount(username,key) > 10:
+            popped = self.lru_dict[username][key]
+            del self.lru_dict[username][key]
+            self.persistent_key[username].add(key)
+            self.presistent_store[username][key] = popped
+
+    def deleteFromPersistent(self, username, key):
+        self.space_dict -= sys.getsizeof(self.presistent_store[username][key])
+        del self.presistent_store[username][key]
+        self.persistent_key[username].remove(key)
+
+    def delete_user(self, username):
+        del self.persistent_key[username]
+        del self.lru_dict[username]
+        del self.space_dict[username]
+        del self.presistent_store[username]
+        del self.count_dict[username]
+
+    def get_key(self,username, key):
+        if key not in self.lru_dict[username]:
+            if key not in self.persistent_key[username]:
+                return - 1
+
+        if key in self.persistent_key[username]:
+            return self.presistent_store[username][key]
+
+        if key in self.lru_dict[username]:
+            self.lru_dict[username].move_to_end(key)
+            self.count_dict[username] += 1
+            return self.lru_dict[username][key]
 
 
-# TODO: record the number of get on a specific key, put it into persistent_dict if it surpasses GET_COUNT_THRESHOLD
-# TODO: update capacity for a user
-def add_user(username, capacity):
-    persistent_dict[username] = set()
-    lru_dict[username] = OrderedDict()
-    capacity_dict[username] = capacity * 1024 * 1024
-    space_dict[username] = 0
-
-
-def delete_user(username):
-    del persistent_dict[username]
-    del lru_dict[username]
-
-
-def update_capacity(username, capacity):
-    pass
-
-
-def get_key(username, key):
-    if key not in lru_dict[username]:
-        return - 1
-
-    lru_dict[username].move_to_end(key)
-    return lru_dict[username][key]
-
-
-def put_key(username, key, value):
-    if key in lru_dict[username]:
-        lru_dict[username].move_to_end(key)
-        space_dict[username] -= sys.getsizeof(lru_dict[username][key])
-
-    lru_dict[username][key] = value
-    space_dict[username] += sys.getsizeof(value)
-
-    if space_dict[username] > capacity_dict[username]:
-        while space_dict[username] > capacity_dict[username]:
-            item = lru_dict[username].popitem(last=False)
-            space_dict[username] -= sys.getsizeof(item[1])
-        return "OK"
-    else:
-        return True
 
