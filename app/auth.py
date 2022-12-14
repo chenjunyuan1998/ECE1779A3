91cc53475e8039a2404107b180a12dcd27b8494a
Tiantian Lin <lintiantian0702@gmail.com>
2022-12-14 01:49:36 -0500

    [Tian] Update frontend

--- app/auth.py
+++ app/auth.py
@@ -1,8 +1,11 @@
+import requests
 from apscheduler.schedulers.background import BackgroundScheduler
-from flask import render_template, url_for, request, redirect
+from flask import render_template, url_for, request, redirect, flash
 from app import webapp
 from flask_login import login_required, current_user
 from flask import json
+
+from app.profile import cache_http
 
 
 @webapp.route('/')
@@ -44,17 +47,19 @@ def register():
 
 
 @webapp.route('/logout',methods =['POST'])
-def logout():
-    return render_template('login.html')
+def logout():#done
+    return redirect(url_for('login'))
 
 @webapp.route('/close_account',methods =['POST'])
-def close_account():
+def close_account():#done
     username = request.form.get('username')
-    password = request.form.get('password')
-
-    if DELETED_USER:
-        msg = 'Delete successfully !'
-        return render_template('login.html', msg=msg)
+    req = {
+        'username': username,
+    }
+    resp = requests.post(cache_http + '/deleteUser', json=req)
+    if resp.json() == 'OK':
+        flash('Delete successfully !')
+        return redirect(url_for('login'))
     else:
         msg = 'Fail to delete!'
         return render_template('profile.html', msg=msg)
--- app/profile.py
+++ app/profile.py
@@ -1,9 +1,10 @@
-
+import requests
 from flask import render_template, url_for, request, flash, redirect
 
 from app import webapp
 from flask import json
 import os
+cache_http = 'http://localhost:5001'
 
 @webapp.route('/profile', methods=['GET'])
 def profile():
@@ -11,34 +12,60 @@ def profile():
 
 
 @webapp.route('/upload', methods=['GET', 'POST'])
-def upload():
+def upload():#done
     if request.method == 'POST':
         key = request.form.get('key')
         new_image = request.files['file']
         username = request.form.get('username')
-        if UPLOADED:
-            return render_template('profile.html', status = 'Uploaded', user = user)
+        req = {
+            'key': key,
+            'username': username,
+            'value': new_image,
+        }
+        resp = requests.post(cache_http + '/put', json=req)
+        if resp.json() == 'OK':
+            return render_template('profile.html', status='Uploaded', user=user)
         else:
             return render_template('profile.html', status='Fail to Upload', user=user)
 
 
 @webapp.route('/config', methods=['GET', 'POST'])
-def config():
+def config():#done
     if request.method == 'POST':
         capacity = request.form.get('capacity')
         username = request.form.get('username')
-        if SUCCESS:
-            return render_template('profile.html', status = 'Capacity Set', user = user)
+        req = {
+            'capacity': capacity,
+            'username': username,
+        }
+        resp = requests.post(cache_http + '/setCap', json=req)
+        if resp.json() == 'OK':
+            return render_template('profile.html', status='Capacity Set')
         else:
-            return render_template('profile.html', status='Fail to Set', user=user)
+            return render_template('profile.html', status='Fail to Set')
 
 @webapp.route('/view_all_image', methods=['GET', 'POST'])
-def view_all_image():
+def view_all_image():#done
      username = request.form.get('username')
-     return render_template('view.html', user = user, image=image, key=key)
+     req = {
+         'username': username,
+     }
+     resp = requests.post(cache_http + '/showGallery', json=req)
+     if resp:
+        return render_template('view.html', items=resp)
+     else:
+        return render_template('view.html')
 
 @webapp.route('/delete_image', methods=['GET', 'POST'])
-def delete_image():
+def delete_image():#done
      username = request.form.get('username')
      key = request.form.get('key')
-     return render_template('view.html', user = user, key_mage=key_image)
+     req = {
+         'username': username,
+         'key': key,
+     }
+     resp = requests.post(cache_http + '/deleteValue', json=req)
+     if resp.json() == 'OK':
+        return redirect('/view_all_image')
+     else:
+        return render_template('view.html', status='Fail to delete')
