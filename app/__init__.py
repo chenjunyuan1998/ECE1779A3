886356f12e285917334d5d00c1408d3ed172f1ea
Tiantian Lin <lintiantian0702@gmail.com>
2022-12-15 10:07:11 -0500

    [Tian] Update flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,50 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from boto3.dynamodb.conditions import Key
 from flask import Flask
-
+from flask_login import LoginManager, UserMixin
 
 #global memcache
+from Backend.DynamoDB.db_account_restful import credential_table
 
 webapp = Flask(__name__)
+webapp.secret_key = 'secret key abc'
+
+
+def create_app():
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(webapp)
+
+    @login_manager.user_loader
+    #load user from session, can not used for the first time login
+    def loader(user_id):
+        response = credential_table.query(
+            KeyConditionExpression=Key('username').eq(user_id))
+
+        if response["Count"] == 0:
+            return
+        user = User(username=response['Items'][0]["username"], password=response['Items'][0]["password"],
+                    image=response['Items'][0]["image"])
+        return user
+
+
+class User(UserMixin):
+    def __init__(self, username, password, image):
+        self.username = username
+        self.password = password
+        self.image = image
 
 
 
+def get_user(username,password):
+    """
+
+    :param username:
+    :param password:
+    :return: user object that include username, password, image list
+
+    to be complete
+    """
+    #get user from db
+    #user = User.query.filter_by().first()
+    return user
--- app/profile.py
+++ app/profile.py
@@ -1,22 +1,28 @@
 import requests
 from flask import render_template, url_for, request, flash, redirect
+from flask_login import login_required, current_user
 
 from app import webapp
 from flask import json
 import os
 cache_http = 'http://localhost:5001'
+account_http= 'http://localhost:5002'
 
 @webapp.route('/profile', methods=['GET'])
+@login_required
 def profile():
-    return render_template('profile.html', user = user)
+
+    return render_template('profile.html', current_user=current_user)
 
 
 @webapp.route('/upload', methods=['GET', 'POST'])
+@login_required
 def upload():#done
     if request.method == 'POST':
         key = request.form.get('key')
         new_image = request.files['file']
-        username = request.form.get('username')
+        username = current_user.get_id()
+        #username = request.form.get('username')
         req = {
             'key': key,
             'username': username,
@@ -24,16 +30,18 @@ def upload():#done
         }
         resp = requests.post(cache_http + '/put', json=req)
         if resp.json() == 'OK':
-            return render_template('profile.html', status='Uploaded', user=user)
+            return render_template('profile.html', status='Uploaded')
         else:
-            return render_template('profile.html', status='Fail to Upload', user=user)
+            return render_template('profile.html', status='Fail to Upload')
 
 
 @webapp.route('/config', methods=['GET', 'POST'])
+@login_required
 def config():#done
     if request.method == 'POST':
         capacity = request.form.get('capacity')
-        username = request.form.get('username')
+        username = current_user.get_id()
+       #username = request.form.get('username')
         req = {
             'capacity': capacity,
             'username': username,
@@ -45,8 +53,10 @@ def config():#done
             return render_template('profile.html', status='Fail to Set')
 
 @webapp.route('/view_all_image', methods=['GET', 'POST'])
+@login_required
 def view_all_image():#done
-     username = request.form.get('username')
+     username = current_user.get_id()
+     #username = request.form.get('username')
      req = {
          'username': username,
      }
@@ -57,8 +67,10 @@ def view_all_image():#done
         return render_template('view.html')
 
 @webapp.route('/delete_image', methods=['GET', 'POST'])
+@login_required
 def delete_image():#done
-     username = request.form.get('username')
+     username = current_user.get_id()
+     #username = request.form.get('username')
      key = request.form.get('key')
      req = {
          'username': username,
