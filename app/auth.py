cd1860fe04ce19e344e5c273aa3a40911454e76f
Tiantian Lin <lintiantian0702@gmail.com>
2022-12-16 15:11:17 -0500

    [Tian] Update

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 import profile
 
 import requests
-from flask import render_template, url_for, request, redirect, flash, make_response
+from flask import render_template, url_for, request, redirect, make_response,flash
 from app import webapp
 from flask import json
 
@@ -34,6 +34,7 @@ def login():#done
         if resp.json() == 'CORRECT_PWD':
             msg = 'Logged in successfully !'
             resp_space = requests.post(cache_http + '/showSpaceUsed', json=req)
+            print('resp_space:',resp_space)
             resp1 = make_response(render_template('profile.html',user=username, space= resp_space))
             resp1.set_cookie('username', username)
             return resp1
@@ -55,11 +56,12 @@ def register():#done
             'username': username,
             'password': password,
         }
-        resp = requests.post(account_http + '/signUp', json=req)
-        if resp.json() == 'ALREADY_EXISTS':
+        account_resp = requests.post(account_http + '/signUp', json=req)
+        cache_resp = requests.post(account_http + '/addUser', json=req)
+        if account_resp.json() == 'ALREADY_EXISTS':
             msg = 'Account already exists !'
             return render_template('register.html', msg=msg)
-        elif resp.json() == 'INVALID_NAME':
+        elif account_resp.json() == 'INVALID_NAME':
             msg = 'Invalid Username !'
             return render_template('register.html', msg=msg)
         else:
