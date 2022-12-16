1cc008f2ab5fb211c7fc889f4028aafbe49c65c4
Yijing622 <chenyijing0622@163.com>
2022-12-16 01:01:30 -0500

    [Yijing] reordered import.

--- Backend/Storage/__init__.py
+++ Backend/Storage/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 from Backend.Storage.storageHelper import *
-from Backend.Storage.storage_restful import *
 
 webapp = Flask(__name__)
 store = storageInterface()
+
+from Backend.Storage.storage_restful import *
