54f342219165c17734bb87718bd0b7f37be8d4fa
Tiantian Lin <lintiantian0702@gmail.com>
2022-12-16 00:39:22 -0500

    [Tian] Update init

--- Backend/DynamoDB/__init__.py
+++ Backend/DynamoDB/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
 
 webapp = Flask(__name__)
+
+from Backend.DynamoDB.db_account_restful import *
--- Backend/StorageInterface/__init__.py
+++ Backend/StorageInterface/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from Backend.StorageInterface.storageHelper import storageInterface
+from Backend.StorageInterface.storage_restful import *
 
 webapp = Flask(__name__)
 store = storageInterface()
