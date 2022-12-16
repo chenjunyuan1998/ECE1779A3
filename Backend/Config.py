fc923d8d27d12590a994bafbad09793868a82598
Yijing622 <chenyijing0622@163.com>
2022-12-16 02:11:50 -0500

    [Yijing] Modified config.

--- Backend/Config.py
+++ Backend/Config.py
@@ -1,6 +1,6 @@
 aws_config = {
-    'aws_access_key_id': "AKIATYDSNXJ56CAX6MXJ",
-    'aws_secret_access_key': "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",
+    'aws_access_key_id': "AKIA5Y2CAKJBWJWLWXRV",
+    'aws_secret_access_key': "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",
 }
 
 bucket = "ece1779s3-group19"
