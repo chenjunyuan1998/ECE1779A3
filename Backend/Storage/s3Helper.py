9f97e7333992770e36f418cb0357d3e7fae363d1
chenjunyuan1998 <chenjunyuan1998@hotmail.com>
2022-12-16 22:07:18 -0500

    u

--- Backend/Storage/s3Helper.py
+++ Backend/Storage/s3Helper.py
@@ -7,9 +7,9 @@ s3 = boto3.client('s3', region_name='us-east-1', aws_access_key_id=aws_config['a
 
 
 def get_size(value):
-    _, extension = os.path.splitext(value.filename)
-    base64_image = base64.b64encode(value.read())
-    image_size = sys.getsizeof(base64_image)
+    #_, extension = os.path.splitext(value.filename)
+    #base64_image = base64.b64encode(value.read())
+    image_size = sys.getsizeof(value)
     return image_size
 
 
@@ -17,12 +17,13 @@ def put_image_to_s3(user, key, value):
     """
     Put image to s3 bucket.
     """
-    _, extension = os.path.splitext(value.filename)
-    base64_image = base64.b64encode(value.read())
+    #_, extension = os.path.splitext(value.filename)
+    #base64_image = base64.b64encode(value.read())
     image_key = str(user) + "::" + str(key)
     s3.put_object(Body=value, Key=image_key, Bucket=bucket, ContentType='image')
     image_url = 'https://s3.amazonaws.com/' + bucket + '/' + image_key + extension
-    image_size = sys.getsizeof(base64_image)
+    #image_size = sys.getsizeof(base64_image)
+    image_size = sys.getsizeof(value)
     return image_size
 
 
