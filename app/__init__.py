502d11a662280563df80587f632da893def2a5b6
chenjunyuan1998 <chenjunyuan1998@hotmail.com>
2022-12-17 12:43:00 -0500

    Revert "u"
    This reverts commit 38ba54f681b3b5f995f92d2f450e4dd259f157bf.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 
 from boto3.dynamodb.conditions import Key
 from flask import Flask
-from app.auth import auth_routes
+from auth import auth_routes
 from app.profile import profile_routes
 #global memcache
 
