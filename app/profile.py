a1596701de70e739221687f91e4287b6f5d236cc
Tiantian Lin <lintiantian0702@gmail.com>
2022-12-17 00:11:52 -0500

    [Tian] Update profile.py

--- app/profile.py
+++ app/profile.py
@@ -72,7 +72,7 @@ def view_all_image():#done
     req = {
         'username': username,
     }
-    resp = requests.get(cache_http + '/showGallery', json=req).json()
+    resp = requests.get(cache_http + '/showGallery', json=req).text
     print('image_resp:', resp)
     if resp:
         resp1 = make_response(render_template('view.html', items=resp))
